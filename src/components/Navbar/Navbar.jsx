c3569c73d8d2a50fe1bb77392d6d0dcf34043368
Navya Kottakkeel <navyakottakkeel@gmail.com>
2026-09-01 00:48:08 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../assets/symbol.png'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={logo} alt="" className='w-12' />
+    </div>
+  )
+}
+
+export default Navbar
